000b5e9117312bdbf946b693417b21a3afd7955d
AleksandrPanov <sasha1_1997@mail.ru>
2020-04-26 15:41:06 +0300

    added parallel block matrix mult and parallel not block LU

--- LU/include/LU.h
+++ LU/include/LU.h
@@ -1,4 +1,81 @@
-void LU_Decomposition(double * A, double * L, double * U, int n)
-{
+#include <omp.h>
 
+
+#define indx(i, j, n) ((i)*(n)+(j))
+#define MIN(a, b) ((a) <=(b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+
+
+void LU_Decomposition(double *A, double *L, double *U, int n) //матрицы по строкам
+{
+    //  ||LU - A||/||A|| < 0.01
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+        L[indx(i, i, n)] = 1.0;
+
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            L[indx(i, j, n)] = 0.0;
+
+
+#pragma omp parallel for
+    for (int i = 0; i < n*n; i++)
+        U[i] = A[i];
+
+    for (int ved = 0; ved < n; ved++)
+    {
+        double el = U[indx(ved, ved, n)];
+        #pragma omp parallel for
+        for (int i = ved + 1; i < n; i++)
+        {
+            double coeff = U[indx(i, ved, n)] / el;
+            L[indx(i, ved, n)] = coeff;
+            for (int j = ved + 1; j < n; j++)
+                U[indx(i, j, n)] -= coeff * U[indx(ved, j, n)];
+
+        }
+    }
+    #pragma omp parallel for
+    for (int i = 1; i < n; i++)
+        for (int j = 0; j < i; j++)
+            U[indx(i, j, n)] = 0.0;
+}
+
+void multMatrix(double *A, double *B, double *C, int n)
+{
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+        for (int z = 0; z < n; z++)
+#pragma ivdep
+            for (int j = 0; j < n; j++)
+            {
+                C[indx(i, j, n)] += A[indx(i, z, n)] * B[indx(z, j, n)];
+            }
+}
+
+void blockMultMatrix(double *A, double *B, double *C, int n)
+{
+    const int bs = 48;
+    #pragma omp parallel for
+    for (int bi = 0; bi < n; bi += bs)
+        for (int bj = 0; bj < n; bj += bs)
+            for (int bk = 0; bk < n; bk += bs)
+                for (int i = bi; i < MIN(bi + bs, n); i++)
+                    for (int k = bk; k < MIN(bk + bs, n); k++)
+                        #pragma ivdep
+                        for (int j = bj; j < MIN(bj + bs, n); j++)
+                        {
+                            C[i*n + j] += A[i*n + k] * B[k*n + j];
+                        }
+}
+
+double getMaxDiff(double *A, double *B, int n)
+{
+    double tmp = 0.0;
+    for (int i = 0; i < n*n; i++)
+    {
+        tmp = MAX(tmp, ABS(A[i] - B[i]));
+    }
 }
--- LU/src/LU.cpp
+++ LU/src/LU.cpp
@@ -1,5 +1,67 @@
 #include "LU.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void printMatrix(double *A, int n)
+{
+    for (int i = 0; i < n*n; i++)
+    {
+        if (i % n == 0 && i != 0)
+            std::cout << "\n";
+        std::cout << A[i] << " ";
+    }
+    std::cout << "\n";
+    std::cout << "\n";
+}
+void generateMatrix(double *A, int n)
+{
+    for (int i = 0; i < n*n; i++)
+    {
+        A[i] = (std::rand() % 21) / 10.0 - 1.0;
+    }
+}
+void transposeMatrix(double *A, double *B, int n)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            B[indx(j, i, n)] = A[indx(i, j, n)];
+}
 int main()
 {
+    const int n = 2000;
+    double *A, *L, *U, *res;
+    A = new double[n*n];
+    L = new double[n*n];
+    U = new double[n*n];
+    res = new double[n*n];
+    for (int i = 0; i < n*n; i++) 
+    {
+        A[i] = 0.0;
+        res[i] = 0.0;
+    }
+
+    generateMatrix(L, n);
+    transposeMatrix(L, U, n);
+
+    auto t1 = clock();
+    blockMultMatrix(L, U, A, n);
+    auto t2 = clock() - t1;
+    std::cout << t2 << "\n";
+    //multMatrix(L, U, A, n);
+    //auto t3 = clock() - t2;
+    //std::cout << t3 << "\n";
+    //std::cout << t2/(double)t3 << "\n";
+    //printMatrix(A, n);
+    LU_Decomposition(A, L, U, n);
+    auto t3 = clock() - t2;
+    std::cout << t3 << "\n";
+
+    multMatrix(L, U, res, n);
+    //printMatrix(res, n);
+    std::cout << getMaxDiff(res, A, n);
+    //printMatrix(U, n);
+    //LU_Decomposition(A, L, U, n);
+
     return 0;
 }
