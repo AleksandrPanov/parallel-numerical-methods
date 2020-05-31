2f912800e5d4a1f0ca0e33387d70e5b6b54c5684
AleksandrPanov <sasha1_1997@mail.ru>
2020-05-31 23:04:18 +0300

    added lab3

---
+++ lab3/RungeKutta/include/RungeKutta.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <omp.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MIN(a, b) ((a) <=(b) ? (a) : (b))
---
+++ lab3/RungeKutta/src/RungeKutta.cpp
@@ -0,0 +1,10 @@
+#include "RungeKutta.h"
+#include "print_generate.h"
+#include <cstdlib>
+#include <ctime>
+
+
+int main()
+{
+    return 0;
+}
