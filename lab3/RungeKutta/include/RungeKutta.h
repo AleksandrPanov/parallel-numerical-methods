01994e6af41d6519a1e60cc80be8757517aac7e9
AleksandrPanov <sasha1_1997@mail.ru>
2020-06-02 18:03:10 +0300

    added final

--- lab3/RungeKutta/include/RungeKutta.h
+++ lab3/RungeKutta/include/RungeKutta.h
@@ -1,32 +1,48 @@
 #pragma once
+#include <algorithm>
+class heat_task {
+public:
+    int n = 50000; // размер сетки по x
+    int m = 50000; // размер сетки по t
+    double L = 100; // длина стержн€
+    double T = 0.1; // момент времени, в который необходимо аппроксимировать u(x, t)
+    double initial_condition(double x) { // функци€, задающа€ начальное условие
+        return pow(x, 4);
+    }
+    double left_condition(double t) { // функци€, задающа€ граничное условие при x = 0
+        return pow(t, 4);
+    }
+    double right_condition(double t) { // функци€, задающа€ граничное условие при x = L
+        return pow(L, 4) + pow(t, 4);
+    }
+    double f(double x, double t) { // функци€, задающа€ внешнее воздействие
+        return 4 * pow(t, 3) - 12 * pow(x, 2);
+    }
+    double u(double x, double t) {
+        return pow(x, 4) + pow(t, 4);
+    }
+    double error(double v, double x, double t) {
+        return abs(v - u(x, t));
+    }
+};
 
+#include <algorithm>
 #include <omp.h>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-#define MIN(a, b) ((a) <=(b) ? (a) : (b))
+class heat_task;
 
-class heat_task {
-public:
-    double T; // момент времени, в который необходимо аппроксимировать u(x, t)
-    double L; // длина стержн€
-    int n; // размер сетки по x
-    int m; // размер сетки по t
-    double initial_condition(double x); // функци€, задающа€ начальное условие
-    double left_condition(double t); // функци€, задающа€ граничное условие при x = 0
-    double right_condition(double t); // функци€, задающа€ граничное условие при x = L
-    double f(double x, double t); // функци€, задающа€ внешнее воздействие
-};
 void calc_k1(heat_task &task, const double *v, double *k1, double t)
 {
     const int n = task.n;
     const double h = task.L / n;
     const double coeff = 1 / (h*h);
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
-        k1[i] = coeff*(v[i + 1] - 2 * v[i] + v[i - 1]) + task.f(h*i, t);
+        k1[i] = coeff*(v[i + 1] - 2.0 * v[i] + v[i - 1]) + task.f(h*i, t);
     }
 }
 void calc_k2(heat_task &task, const double *v, const double *k1, double *k2, double t, double *tmp)
@@ -36,11 +52,13 @@ void calc_k2(heat_task &task, const double *v, const double *k1, double *k2, dou
     const double coeff = 1 / (h*h);
     const double dt = task.T/(task.m * 2.0);
     tmp[0] = task.left_condition(t);
+    tmp[n] = task.right_condition(t);
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
         tmp[i] = v[i] + dt * k1[i];
     }
-    tmp[n] = task.right_condition(t);
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
         k2[i] = coeff * (tmp[i + 1] - 2 * tmp[i] + tmp[i - 1]) + task.f(h*i, t);
@@ -53,12 +71,14 @@ void calc_k3(heat_task &task, const double *v, const double *k2, double *k3, dou
     const double h = task.L / n;
     const double coeff = 1 / (h*h);
     const double dt = task.T / (task.m * 2.0);
+    tmp[0] = task.left_condition(t);
+    tmp[n] = task.right_condition(t);
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
         tmp[i] = v[i] + dt * k2[i];
     }
-    tmp[0] = task.left_condition(t);
-    tmp[n] = task.right_condition(t);
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
         k3[i] = coeff * (tmp[i + 1] - 2 * tmp[i] + tmp[i - 1]) + task.f(h*i, t);
@@ -70,10 +90,14 @@ void calc_k4(heat_task &task, const double *v, const double *k3, double *k4, dou
     const double h = task.L / n;
     const double dt = task.T / task.m;
     const double coeff = 1 / (h*h);
-    for (int i = 0; i < n; i++)
+    tmp[0] = task.left_condition(t);
+    tmp[n] = task.right_condition(t);
+    #pragma omp parallel for
+    for (int i = 1; i < n; i++)
     {
         tmp[i] = v[i] + dt * k3[i];
     }
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
         k4[i] = coeff * (tmp[i + 1] - 2 * tmp[i] + tmp[i - 1]) + task.f(h*i, t);
@@ -83,13 +107,13 @@ void calc_k4(heat_task &task, const double *v, const double *k3, double *k4, dou
 void calc(heat_task &task, double *v, const double *k1, const double *k2, const double *k3, const double *k4, const double curTime)
 {
     const int n = task.n;
-    const double dt = task.T / (task.m * 6.0);
+    const double dt = task.T / task.m;
+    const double coeff = dt / 6.0;
+    #pragma omp parallel for
     for (int i = 1; i < n; i++)
     {
-        v[i] += dt * (k1[i] + k2[i] + k3[i] + k4[i]);
+        v[i] += coeff * (k1[i] + 2.0*k2[i] + 2.0*k3[i] + k4[i]);
     }
-    v[0] = task.left_condition(curTime);
-    v[n] = task.right_condition(curTime);
 }
 void heat_equation_runge_kutta(heat_task task, double * v)//v Ц указатель на массив размера n+1, в который необходимо записать численное решение в момент t
 {
@@ -110,18 +134,18 @@ void heat_equation_runge_kutta(heat_task task, double * v)//v Ц указатель на мас
     {
         v[i] = task.initial_condition(i*h); //x_i = i*h
     }
-    v[0] = task.left_condition(curTime);
-    v[n] = task.right_condition(curTime);
-    for (int tick = 1; tick < m + 1; tick++)
+    for (int tick = 0; tick < m; tick++)
     {
         curTime = dt * tick;
+        v[0] = task.left_condition(curTime);
+        v[n] = task.right_condition(curTime);
         calc_k1(task, v, k1, curTime);
         calc_k2(task, v, k1, k2, curTime + dt/2, tmp.data());
         calc_k3(task, v, k2, k3, curTime + dt/2, tmp.data());
         calc_k4(task, v, k3, k4, curTime + dt, tmp.data());
-        calc(task, v, k1, k2, k3, k3, curTime);
+        calc(task, v, k1, k2, k3, k4, curTime);
     }
+    v[0] = task.left_condition(curTime + dt);
+    v[n] = task.right_condition(curTime + dt);
 }
-// ‘ункци€ получает в аргументах следующие переменные :
-// task Ц экземепл€р класса типа heat_task, в котором хранитс€ описание задачи.
-// √арантируетс€ выполнение услови€ на сходимость метода на входных данных.
+
--- lab3/test_RungeKutta/src/main.cpp
+++ lab3/test_RungeKutta/src/main.cpp
@@ -1,6 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <omp.h>
 #include <ctime>
+#include "RungeKutta.h"
 
-int main()
+double error(heat_task task, double * v) 
 {
+    double err = 0;
+    for (int i = 0; i < task.n + 1; i++) 
+    {
+        err = max(err, task.error(v[i], i*(task.L / task.n), task.T));
+    }
+    return err;
+}
+
+int main() 
+{
+    omp_set_num_threads(4);
+    heat_task task;
+    if (task.m < 2 * task.T*pow((task.n / task.L), 2)) 
+    {
+        cout << "m should be more than " << 2 * task.T*pow((task.n / task.L), 2) << "\n";
+        return 1;
+    }
+
+    double * v = new double[task.n + 1];
+
+    int time1 = clock();
+    heat_equation_runge_kutta(task, v);
+    int time2 = clock() - time1;
+
+    cout << time2 << " n = " << task.n << " , m = " << task.m << " , error = " << error(task, v) << "\n";
+
     return 0;
 }
